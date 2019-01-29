1364b722b1e1dd45a23e80f25ce1adca7b8f7cc4
Szeba25 <lustmord20@gmail.com>
2019-01-29 16:25:34 +0100

    Added documentation to heap and nodes

--- Assets/Scripts/AI/Pathfinder/MinHeap.cs
+++ Assets/Scripts/AI/Pathfinder/MinHeap.cs
@@ -10,11 +10,17 @@ namespace Anais {
         private Node[] queue;
         private int size;
 
+        /// <summary>
+        /// Constructs a new empty minimum heap.
+        /// </summary>
         public MinHeap() {
             queue = new Node[INITIAL_CAPACITY];
             size = 0;
         }
 
+        /// <summary>
+        /// Clears the heap for later reuse, but does not reset the capacity.
+        /// </summary>
         public void Clear() {
             for (int i = 1; i <= size; i++) {
                 queue[i] = null;
@@ -22,6 +28,10 @@ namespace Anais {
             size = 0;
         }
 
+        /// <summary>
+        /// Add a new node to the heap.
+        /// </summary>
+        /// <param name="node"></param>
         public void Add(Node node) {
             size++;
             Grow(size + 1);
@@ -30,10 +40,18 @@ namespace Anais {
             BubbleUp(size);
         }
 
+        /// <summary>
+        /// Peek at the first (lowest cost node) at the heap.
+        /// </summary>
+        /// <returns>The lowest cost node</returns>
         public Node Peek() {
             return queue[1];
         }
 
+        /// <summary>
+        /// Peek, and remove the lowest cost node.
+        /// </summary>
+        /// <returns>The lowest cost node</returns>
         public Node Poll() {
             Node minNode = Peek();
             queue[1] = queue[size];
@@ -44,34 +62,71 @@ namespace Anais {
             return minNode;
         }
 
+        /// <summary>
+        /// Try to move the node at this index "up" as much as possible.
+        /// </summary>
+        /// <param name="i"></param>
         public void Decrease(int i) {
             BubbleUp(i);
         }
 
+        /// <summary>
+        /// Check if the heap is empty.
+        /// </summary>
+        /// <returns>True, if the heap is empty</returns>
         public bool Empty() {
             return size == 0;
         }
 
+        /// <summary>
+        /// Gets the parent index of i.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns>The parent index of i</returns>
         private int Parent(int i) {
             return i / 2;
         }
 
+        /// <summary>
+        /// Gets the left child index of i.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns>The left child index of i</returns>
         private int LeftChild(int i) {
             return i * 2;
         }
 
+        /// <summary>
+        /// Gets the right child index of i.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
         private int RightChild(int i) {
             return (i * 2) + 1;
         }
 
+        /// <summary>
+        /// Check if i has a left child in the heap.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns>True if i has a left child</returns>
         private bool HasLeftChild(int i) {
             return LeftChild(i) <= size;
         }
 
+        /// <summary>
+        /// Check if i has a right child in the heap.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns>True if i has a right child</returns>
         private bool HasRightChild(int i) {
             return RightChild(i) <= size;
         }
 
+        /// <summary>
+        /// Try to move i up as much as possible (bubbling up the node).
+        /// </summary>
+        /// <param name="i"></param>
         private void BubbleUp(int i) {
             while (i > 1 && queue[Parent(i)].TotalCost > queue[i].TotalCost) {
                 Swap(i, Parent(i));
@@ -79,6 +134,10 @@ namespace Anais {
             }
         }
 
+        /// <summary>
+        /// Try to move i down as much as possible (bubbling down the node).
+        /// </summary>
+        /// <param name="i"></param>
         private void BubbleDown(int i) {
             int swapWith = i;
             int current = 0;
@@ -100,6 +159,11 @@ namespace Anais {
             }
         }
 
+        /// <summary>
+        /// Swap two nodes at i and j indices.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
         private void Swap(int i, int j) {
             Node temp1 = queue[i];
             Node temp2 = queue[j];
@@ -109,6 +173,11 @@ namespace Anais {
             temp2.Index = i;
         }
 
+        /// <summary>
+        /// Grow the heap, if its capacity is too low.
+        /// </summary>
+        /// <param name="minCapacity"></param>
+        /// <returns></returns>
         public bool Grow(int minCapacity) {
             if (queue.Length < minCapacity) {
                 int newCapacity = queue.Length * 2;
@@ -122,6 +191,9 @@ namespace Anais {
             }
         }
 
+        /// <summary>
+        /// Check for heap errors. Useful if "something" funky happens.
+        /// </summary>
         public void CheckForErrors() {
             for (int i = 1; i <= size; i++) {
                 if (queue[i].Index != i) {
--- Assets/Scripts/AI/Pathfinder/Node.cs
+++ Assets/Scripts/AI/Pathfinder/Node.cs
@@ -13,6 +13,11 @@
         public int ExtraCost { get; set; }
         public int Index { get; set; }
 
+        /// <summary>
+        /// Create a node at x and y coordinates, with default parameters.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public Node(int x, int y) {
             X = x;
             Y = y;
@@ -24,6 +29,13 @@
             Index = -1;
         }
 
+        /// <summary>
+        /// Initialize the node with a custom parameter set.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="totalCost"></param>
+        /// <param name="extraCost"></param>
+        /// <param name="index"></param>
         public void Initialize(Node parent, int totalCost, int extraCost, int index) {
             Parent = parent;
             TotalCost = totalCost;
@@ -31,6 +43,9 @@
             Index = index;
         }
 
+        /// <summary>
+        /// Initialize the node with the maximum possible cost.
+        /// </summary>
         public void InitializeMaximized() {
             Parent = null;
             TotalCost = int.MaxValue;
@@ -38,6 +53,9 @@
             Index = -1;
         }
 
+        /// <summary>
+        /// Initialize the node with the minimum possible cost.
+        /// </summary>
         public void InitializeMinimized() {
             Parent = null;
             TotalCost = 0;
